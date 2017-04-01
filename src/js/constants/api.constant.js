2a847a35d931ed75df6cc612bab5c80c0a925f5f
Josua Amazigh <josua@jypino.eu>
2017-04-01 11:50:13 +0200

    URL API modified

--- src/js/constants/api.constant.js
+++ src/js/constants/api.constant.js
@@ -1,3 +1,3 @@
 angular
   .module('Alfoorsa')
-  .constant('API', 'http://ec2-54-229-121-200.eu-west-1.compute.amazonaws.com:3000');
+  .constant('API', 'ec2-54-246-195-2.eu-west-1.compute.amazonaws.com');
