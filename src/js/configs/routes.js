d34064dc4b67919547c97891a3a4c051661ae416
Josua Amazigh <josua@jypino.eu>
2017-04-06 14:08:47 +0200

    check if the user logged is an Admin in the stateProviderODODODODODlogged user has the ADMIN role in stthe stateProvider

--- src/js/configs/routes.js
+++ src/js/configs/routes.js
@@ -2,8 +2,8 @@ angular
 .module('Alfoorsa')
 .config(Router);
 
-Router.$inject = ['$stateProvider', '$locationProvider', '$urlRouterProvider', 'CurrentUserServiceProvider'];
-function Router($stateProvider, $locationProvider, $urlRouterProvider, CurrentUserServiceProvider){
+Router.$inject = ['$stateProvider', '$locationProvider', '$urlRouterProvider'];
+function Router($stateProvider, $locationProvider, $urlRouterProvider){
   $locationProvider.html5Mode(true);
 
   $stateProvider
@@ -14,39 +14,25 @@ function Router($stateProvider, $locationProvider, $urlRouterProvider, CurrentUs
   .state('session', {
     url: '/session',
     templateUrl: '/js/views/session.html'
+  })
+  .state('erdamatuput', {
+    url: '/eraseunavezalguienfamoso',
+    templateUrl: '/js/views/famosos/los_famosos.html',
+    controller: 'famososCtrl',
+    controllerAS: 'famosos',
+    resolve: {
+      validate: function($q, $location, CurrentUserService) {
+        var validateAccess = $q.defer();
+        if (CurrentUserService !== null && CurrentUserService.currentUser !== null && CurrentUserService.currentUser.role === 'ADMIN') {
+          validateAccess.resolve();
+        }else{
+          validateAccess.reject();
+          $location.url('/session');
+        }
+        return validateAccess.promise;
+      }
+    }
   });
-  // .state('erdamatuput', {
-  //   url: '/eraseunavezalguienfamoso',
-  //   templateUrl: '/js/views/famosos/los_famosos.html',
-  //   controller: 'famososCtrl',
-  //   controllerAS: 'famosos'
-    // resolve: {
-    //   validate: function($q, $location, CurrentUserService) {
-    //     // Either you could maintain an array of hashes on client side
-    //     // a user do not have access to without login
-    //     // Or hit the backend url to check it more securely
-    //     //console.log(CurrentUserService.currentUser);
-    //     var validateAccess = $q.defer();
-    //     // var isAllowed = ['profile', 'index', 'dashboard'].indexOf($location.hash()) !== -1;
-    //     //
-    //     // if (!isAllowed) {
-    //     //   $location.path('/login');
-    //     // }
-    //
-    //     validateAccess.resolve();
-    //     return validateAccess.promise;
-    //   }
-    // }
-  // });
-
-  $urlRouterProvider.when('/eraseunavezalguienfamoso', ['$match', function ($match) {
-    console.log('hola');
-    console.log(CurrentUserServiceProvider)
-    // if (window.location.replace(window.location.href, '') == 'eraseunavezalguienfamoso') {
-    //     //$state.transitionTo(state, $match, false);
-    //     console.log(CurrentUserService.currentUser);
-    // }
-  }]);
   // .state('profile', {
   //   url: '/profile',
   //   templateUrl: '',
